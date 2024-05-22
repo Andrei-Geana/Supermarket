2c1803dc03c18060fca84b83fc0c6e6d66ae547d
andreigeana71 <niculae.geana@student.unitbv.ro>
2024-05-23 01:52:59 +0300

    Add business logic for login.

---
+++ SupermarketApp/SupermarketApp/Models/BusinessLogic/RoleBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketApp.Models.BusinessLogic
+{
+    public class RoleBLL
+    {
+        private static readonly string _adminString = "ADMIN";
+        private static readonly string _cashierString = "CASHIER";
+
+        private SupermarketMAPEntities entities = new SupermarketMAPEntities();
+
+        public RoleBLL() { }
+
+        public int GetIdOfAdmin()
+        {
+            return entities.Roles.Where(role => role.name == _adminString).First().id;
+        }
+    }
+}
---
+++ SupermarketApp/SupermarketApp/Models/BusinessLogic/UserBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketApp.Models.BusinessLogic
+{
+    public class UserBLL
+    {
+        private SupermarketMAPEntities entities = new SupermarketMAPEntities();
+
+        public UserBLL() { }
+
+        public User Login(User user)
+        {
+            User u = entities.Users.Where(mock => mock.password == user.password).Where(mock => mock.name == user.name).FirstOrDefault();
+            return u;
+        }
+    }
+}
