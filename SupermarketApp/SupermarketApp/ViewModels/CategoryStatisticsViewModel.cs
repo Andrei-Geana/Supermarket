c812c0de878cbbc6a7b6d339261dfec329d9a564
andreigeana71 <niculae.geana@student.unitbv.ro>
2024-05-27 11:42:41 +0300

    Add functionality to CategoryStatistics admin menu. Update Receipts view.

--- SupermarketApp/SupermarketApp/ViewModels/CategoryStatisticsViewModel.cs
+++ SupermarketApp/SupermarketApp/ViewModels/CategoryStatisticsViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using SupermarketApp.Commands;
+using SupermarketApp.Models;
 using SupermarketApp.Models.BusinessLogic;
 using SupermarketApp.Stores;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +14,28 @@ namespace SupermarketApp.ViewModels
 {
     public class CategoryStatisticsViewModel : ViewModelBase
     {
+        private ProductCategoryBLL _productCategoryBLL;
+        private ObservableCollection<GetCategoryValue_Result> _results;
         public CategoryStatisticsViewModel(Navigation navigation, Func<MainMenuViewModel> createMainMenu, Func<ProviderStatisticsViewModel> createProviderStatisticsMenu,
             Func<StatisticsViewModel> createEarningsMenu)
         {
             NavigateBackToMenu = new NavigationCommand(navigation, createMainMenu);
             NavigateToProviderStatistics = new NavigationCommand(navigation, createProviderStatisticsMenu);
             NavigateToEarningsStatistics = new NavigationCommand(navigation, createEarningsMenu);
+
+            _productCategoryBLL = new ProductCategoryBLL();
+
             ResetSelection();
         }
 
         private void ResetSelection()
         {
+            Results = _productCategoryBLL.GetCategoryStatistics();
         }
 
         public ICommand NavigateBackToMenu { get; set; }
         public ICommand NavigateToProviderStatistics { get; set; }
         public ICommand NavigateToEarningsStatistics { get; set; }
+        public ObservableCollection<GetCategoryValue_Result> Results { get => _results; set => _results = value; }
     }
 }
